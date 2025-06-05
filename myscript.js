2edc1c5348e70f9239e51d19eb4284d8269bdd48
Narayan7330 <narayanmali7330@gmail.com>
2025-06-05 16:57:00 +0000

    made an hover color changing function

--- myscript.js
+++ myscript.js
@@ -5,5 +5,10 @@ for(let i = 1;i <= 256;i++){
   // div.style.cssText = "border:1px solid black; height:41.76px;width:41.76px";
   div.classList.add("squareDiv");
   container.appendChild(div);
+
+  div.addEventListener('mouseover',()=> {
+    div.style.backgroundColor = "black";
+  })
 }
+
 
