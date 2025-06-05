c7cad82be5768f31ed5a881924988e7df9eeb2b5
Narayan7330 <narayanmali7330@gmail.com>
2025-06-05 14:41:45 +0000

    made 16 x 16 grid

---
+++ myscript.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("#container")
+
+for(let i = 1;i <= 256;i++){
+  const div = document.createElement('div');
+  // div.style.cssText = "border:1px solid black; height:41.76px;width:41.76px";
+  div.classList.add("squareDiv");
+  container.appendChild(div);
+}
+
